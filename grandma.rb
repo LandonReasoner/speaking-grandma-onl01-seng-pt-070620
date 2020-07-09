3705f56e42990dc26a27303d7e6e9e97de93d4cc
Landon <reasoner.landon90@gmail.com>
2020-07-09 02:09:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,10 @@
+
 def speak_to_grandma(phrase)
-  phrase
-end
-
-
-if phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "ILOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == phrase.upcase && phrase! = "I LOVE YOU GRANDMA!"
+  elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-else phrase == phrase.downcase
-    puts "HUH?! SPEAK UP SONNY!"
+  else phrase 
+    return "HUH?! SPEAK UP SONNY!"
+  end
 end
